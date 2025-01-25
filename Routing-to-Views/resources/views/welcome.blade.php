157bcbe7c1259683e26a3de2a0f2218d4f0662a3
Leif Hurei Jose <152266727+Feilieruhesoj@users.noreply.github.com>
2025-01-25 14:23:48 +0000

    Adding routes to view

---
+++ Routing-to-Views/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcom Page</title>
+</head>
+<body>
+    <h1>Hello, Laravel!</h1>
+    <a href="/greet" class="btn">Greetings?</a>
+</body>
+</html>
